b31a9046998662b5f2bf2ef13c8a197610a1193b
feldhaus <maicon.feldhaus@gmail.com>
2017-03-03 10:06:06 -0300

    Added game files

---
+++ js/ai.js
@@ -0,0 +1,162 @@
+/*jslint vars:true, devel:true, plusplus:true*/
+
+var AI = {
+    
+    // depth-first search
+    dfs: function (grid, playerPosition, startPlayerPosition, size) {
+
+        var start = this.getState(grid, playerPosition);
+        
+        var frontier = [start.id];
+        var explored = {[start.id]: false};
+        var data = {[start.id]: start};
+        
+        while (frontier.length > 0) {
+            
+            var current = data[frontier.pop()]
+            explored[current.id] = true;
+            
+            var total = this.getTotal(current.grid);
+            if (total == 0) {
+                return this.reconstructPath(start, current);
+            }
+            
+            var neighbors = this.getNeighbors(current.grid, current.position, startPlayerPosition, size);
+            for (var i = 0; i < neighbors.length; i++) {
+                var neighbor = neighbors[i];
+                if (data[neighbor.id] === undefined && explored[neighbor.id] === undefined) {
+                    neighbor.parent = current;
+                    frontier.push(neighbor.id);
+                    data[neighbor.id] = neighbor;
+                }
+            }
+        }
+        return false;
+    },
+    
+    ast: function (grid, playerPosition, startPlayerPosition, size) {
+        
+        var start = this.getState(grid, playerPosition);
+        start.priority = 0;
+        
+        var frontier = new PriorityQueue({comparator: function(a, b) { return a.priority - b.priority; }});
+        frontier.queue(start);
+        var costSoFar = {[start.id]: 0};
+        
+        this.debug(start.grid, size);
+         
+        while (frontier.length > 0) {
+            current = frontier.dequeue();
+             
+            var total = this.getTotal(current.grid);
+            if (total === 0) {
+                return this.reconstructPath(start, current);
+            }
+             
+            var neighbors = this.getNeighbors(current.grid, current.position, startPlayerPosition, size);
+            for (var i = 0; i < neighbors.length; i++) {
+                 
+                var newCost = costSoFar[current.id] + 1;
+                var neighbor = neighbors[i];
+                
+                if (costSoFar[neighbor.id] === undefined || newCost < costSoFar[neighbor.id]) {
+                    costSoFar[neighbor.id] = newCost;
+                    neighbor.parent = current;
+                    var priority = newCost + this.getTotal(neighbors[i].grid);
+                    neighbor.priority = priority;
+                    frontier.queue(neighbor);
+                }
+            }
+        }
+    },
+    
+    getState: function (grid, position) {
+        return {
+            id: this.getId(grid, position),
+            grid: grid,
+            position: position,
+            parent: null
+        };
+    },
+    
+    //
+    getTotal: function (grid) {
+        var sum = 0;
+        for (var i = 0; i < grid.length; i++) {
+            if (grid[i] > 0) {
+                sum += grid[i];
+            }
+        }
+        return sum;
+    },
+    
+    //
+    getId: function (grid, pos) {
+        var id = "(" + pos.x + "," + pos.y + ") ";
+        for (var i = 0; i < grid.length; i++) {
+            id += grid[i];
+        }
+        return id;
+    },
+    
+    inBounds: function (node, size) {
+        return node.row >= 0 && node.row < size.rows && node.row >= 0 && node.col < size.cols;
+    },
+    
+    // get all neighbors and filter them
+    getNeighbors: function(grid, position, startPosition, size) {
+        var possible = [
+            {col: position.x, row: position.y - 1},
+            {col: position.x, row: position.y + 1},
+            {col: position.x - 1, row: position.y},
+            {col: position.x + 1, row: position.y}
+        ];
+        
+        var neighbors = [];
+        for (var i = 0; i < possible.length; i++) {
+            var n = possible[i];
+            
+            if (this.inBounds(n, size) && (grid[n.row * size.cols + n.col] > 0 || (n.row == startPosition.y && n.col == startPosition.x))) {
+                var clone = grid.slice(0);
+                clone[position.y * size.cols + position.x]--;
+                neighbors.push(this.getState(clone, new Phaser.Point(n.col, n.row)));
+            }
+        }
+        
+        return neighbors;
+    },
+    
+    reconstructPath: function (start, goal) {
+        var current = goal;
+        var path = [];
+        
+        while (current.id !== start.id) {
+            if (current.position.y < current.parent.position.y) {
+                path.push('up');
+            } else if (current.position.y > current.parent.position.y) {
+                path.push('down');
+            } else if (current.position.x < current.parent.position.x) {
+                path.push('left');
+            } else if (current.position.x > current.parent.position.x) {
+                path.push('right');
+            }
+            current = current.parent;
+        }
+        
+        path.reverse();
+        
+        return path;
+    },
+    
+    debug: function (grid, size) {
+        console.log('  | 0 1 2 3 4 5 6 7 8 9');
+        console.log('-------------------------------------');
+        for (var i = 0; i < size.rows; i++) {
+            var line = i + ' | ';
+            for (var j = 0; j < size.cols; j++) {
+                line += grid[i * size.cols + j] + ' ';
+            }
+            console.log(line);
+        }
+    }
+};
---
+++ js/game.js
@@ -0,0 +1,394 @@
+/*jslint vars:true, devel:true, plusplus:true*/
+/*global Phaser*/
+
+var game;
+
+// main game options
+var gameOptions = {
+    gameWidth: 1000,
+    gameHeight: 800,
+    tileSize: 100
+};
+
+// game levels
+var levels = [
+    {
+        level: [
+            [1, 1, 1, 1],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [1, 1, 1, 1],
+        ],
+        playerPos: new Phaser.Point(0, 0)
+    },
+    {
+        level: [
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1]
+        ],
+        playerPos: new Phaser.Point(0, 0)
+    },
+    {
+        level: [
+            [1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 1],
+            [1, 0, 1, 0, 1],
+            [1, 1, 2, 1, 1]
+        ],
+        playerPos: new Phaser.Point(2, 2)
+    },
+    {
+        level: [
+            [1, 1, 1, 0, 0],
+            [1, 0, 1, 0, 0],
+            [1, 1, 2, 1, 1],
+            [0, 0, 1, 0, 1],
+            [0, 0, 1, 1, 1],
+            [0, 0, 0, 0, 0]
+        ],
+        playerPos: new Phaser.Point(2, 0)
+    },
+    {
+        level: [
+            [1, 1, 2, 1, 1],
+            [1, 0, 1, 0, 1],
+            [1, 2, 2, 2, 1],
+            [0, 1, 0, 1, 0],
+            [0, 1, 1, 1, 0]
+        ],
+        playerPos: new Phaser.Point(2, 1)
+    },
+    {
+        level: [
+            [0, 1, 1, 1, 1, 1, 1],
+            [0, 1, 1, 1, 1, 1, 1],
+            [0, 0, 1, 1, 1, 1, 0],
+            [1, 1, 1, 1, 1, 1, 0],
+            [1, 1, 1, 1, 1, 1, 0],
+            [0, 0, 0, 0, 1, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0]
+        ],
+        playerPos: new Phaser.Point(4, 1)
+    },
+    {
+        level: [
+            [0, 0, 0, 0, 1, 1, 1, 1],
+            [1, 1, 0, 0, 1, 1, 1, 1],
+            [1, 1, 2, 2, 1, 1, 1, 0],
+            [1, 1, 2, 2, 1, 1, 1, 0],
+            [0, 0, 0, 0, 0, 2, 2, 0],
+            [0, 0, 0, 0, 0, 2, 2, 0],
+            [0, 0, 0, 0, 0, 1, 1, 0]
+        ],
+        playerPos: new Phaser.Point(4, 2)
+    }
+];
+
+// current level
+var levelNumber = 0;
+
+// when the window finishes loading...
+window.onload = function () {
+    game = new Phaser.Game(gameOptions.gameWidth, gameOptions.gameHeight);
+    game.state.add('TheGame', TheGame);
+    game.state.start('TheGame');
+};
+
+var TheGame = function () {};
+
+TheGame.prototype = {
+    
+    rows: 0,
+    cols: 0,
+
+    // preloading assets
+    preload: function () {
+        game.load.spritesheet('tiles', 'assets/images/tilesheet.png', 128, 128);
+    },
+
+    // when the game starts
+    create: function () {
+        // set game scale
+        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        game.scale.pageAlignHorizontally = true;
+        game.scale.pageAlignVertically = true;
+        
+        // create level
+        this.createLevel();
+        
+        // define inputs (touch and keyboard)
+        game.input.onDown.add(this.beginSwipe, this);
+        var keyUp = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+        keyUp.onDown.add(this.moveUp, this);
+        var keyDown = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+        keyDown.onDown.add(this.moveDown, this);
+        var keyLeft = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+        keyLeft.onDown.add(this.moveLeft, this);
+        var keyRight = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+        keyRight.onDown.add(this.moveRight, this);
+        var keySpace = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        keySpace.onDown.add(this.automata, this);
+    },
+
+    // function to create a level
+    createLevel: function () {
+        var currentLevel = levels[levelNumber];
+        this.cols = currentLevel.level[0].length;
+        this.rows = currentLevel.level.length;
+        
+        this.tilesArray = [];
+        this.tileGroup = game.add.group();
+        this.tileGroup.x = (game.width - gameOptions.tileSize * this.cols) / 2;
+        this.tileGroup.y = (game.height -  gameOptions.tileSize * this.rows) / 2;
+        
+        for (var i = 0; i < this.rows; i++) {
+            this.tilesArray[i] = [];
+            for (var j = 0; j < this.cols; j++) {
+                if (currentLevel.level[i][j] !== 0) {
+                    this.addTile(i, j, levels[levelNumber].level[i][j]);
+                }
+            }
+        }
+        
+        this.playerPosition = new Phaser.Point(0, 0);
+        levels[levelNumber].playerPos.clone(this.playerPosition);
+        var tilePos = this.getTilePosition(levels[levelNumber].playerPos.y, levels[levelNumber].playerPos.x);
+        this.tilesArray[this.playerPosition.y][this.playerPosition.x].frame = 90;
+        this.player = game.add.sprite(tilePos.x, tilePos.y, 'tiles');
+        this.player.width = gameOptions.tileSize;
+        this.player.height = gameOptions.tileSize;
+        this.player.frame = 65;
+        this.player.anchor.set(0.5);
+        this.tileGroup.add(this.player);
+        this.firstMove = true;
+	},
+
+    // function add a tile at "row" row, "col" column with "val" value
+    addTile: function (row, col, val) {
+        var tilePos = this.getTilePosition(row, col);
+        var theTile = game.add.sprite(tilePos.x, tilePos.y, 'tiles');
+        if (val === 2) {
+            theTile.tint = 0xffff88;
+        }
+        theTile.anchor.set(0.5);
+        theTile.tileValue = val;
+        theTile.width = gameOptions.tileSize;
+        theTile.height = gameOptions.tileSize;
+        theTile.frame = 102;
+        theTile.falling = false;
+        theTile.tilePosition = new Phaser.Point(col, row);
+        this.tilesArray[row][col] = theTile;
+        this.tileGroup.add(theTile);
+        
+        var txt = game.add.text(tilePos.x, tilePos.y, col + ',' + row);
+        this.tileGroup.add(txt);
+    },
+    
+    // return tile position
+    getTilePosition: function (row, col) {
+        return {
+            x: col * gameOptions.tileSize + gameOptions.tileSize / 2,
+            y: row * gameOptions.tileSize + gameOptions.tileSize / 2
+        };
+    },
+    
+    // return a tile
+    getTile: function (row, col) {
+        if (row < 0 || row >= this.rows || col < 0 || col >= this.cols) {
+            return null;
+        }
+        return this.tilesArray[row][col];
+    },
+
+    // start checking for swipes
+    beginSwipe: function (event) {
+        game.input.onDown.remove(this.beginSwipe, this);
+        game.input.onUp.add(this.endSwipe, this);
+    },
+     
+    // end checking for swipes
+    endSwipe: function (event) {
+        game.input.onUp.remove(this.endSwipe, this);
+        var swipeTime = event.timeUp - event.timeDown;
+        var swipeDistance = Phaser.Point.subtract(event.position, event.positionDown);
+        var swipeMagnitude = swipeDistance.getMagnitude();
+        var swipeNormal = Phaser.Point.normalize(swipeDistance);
+        if (swipeMagnitude > 20 && swipeTime < 1000 && (Math.abs(swipeNormal.x) > 0.8 || Math.abs(swipeNormal.y) > 0.8)) {
+            if (swipeNormal.y < -0.8) {
+                this.moveUp();
+            } else if (swipeNormal.y > 0.8) {
+                this.moveDown();
+            } else if (swipeNormal.x < -0.8) {
+                this.moveLeft();
+            } else if (swipeNormal.x > 0.8) {
+                this.moveRight();
+            }
+        } else {
+            game.input.onDown.add(this.beginSwipe, this);
+        }
+    },
+    
+    // move player to up
+    moveUp: function () {
+        this.handleMovement(new Phaser.Point(0, -1));
+        this.player.frame = 68;
+    },
+    
+    // move player to down
+    moveDown: function () {
+        this.handleMovement(new Phaser.Point(0, 1));
+        this.player.frame = 65;
+    },
+    
+    // move player to left
+    moveLeft: function () {
+        this.handleMovement(new Phaser.Point(-1, 0));
+        this.player.frame = 94;
+    },
+    
+    // move player to right
+    moveRight: function () {
+        this.handleMovement(new Phaser.Point(1, 0));
+        this.player.frame = 91;
+    },
+
+    // handling swipes
+    handleMovement: function (position) {
+        var tile = this.getTile(this.playerPosition.y, this.playerPosition.x);
+        
+        if (this.firstMove) {
+            this.firstMove = false;
+            tile.tileValue = 9;
+        } else if (tile) {
+            switch (tile.tileValue) {
+                case 1:
+                    tile.falling = true;
+                    var tween = game.add.tween(tile).to({
+                        alpha: 0,
+                        width: gameOptions.tileSize / 4,
+                        height: gameOptions.tileSize / 4,
+                        angle: game.rnd.integerInRange(-15, 15)
+                    }, 500, Phaser.Easing.Linear.None, true);
+                    tween.onComplete.add(function(target){
+                        this.tilesArray[target.tilePosition.y][target.tilePosition.x] = null;
+                        target.destroy();
+                    }, this);
+                    break;
+                case 2:
+                    tile.tileValue = 1;
+                    tile.tint = 0xffffff;
+                    break;
+            }
+        }
+        
+        this.playerPosition.add(position.x, position.y);
+        tile = this.getTile(this.playerPosition.y, this.playerPosition.x);
+        
+        var playerTween = game.add.tween(this.player).to({
+            x: this.playerPosition.x * gameOptions.tileSize + gameOptions.tileSize / 2,
+            y: this.playerPosition.y * gameOptions.tileSize + gameOptions.tileSize / 2
+        }, 100, Phaser.Easing.Linear.None, true);
+        
+        playerTween.onComplete.add(function(){
+            if (tile == null || tile.falling) {
+                this.levelRestart();
+            } else {
+                if (tile.tileValue == 9) {
+                    if (this.isLevelComplete()) {
+                        if (levelNumber < levels.length) {
+                            levelNumber ++;
+                            this.levelRestart();
+                        } else {
+                            alert('Ok! You completed the game!');
+                        }
+                    } else {
+                        var tween = game.add.tween(tile).to({
+                            alpha: 0,
+                        }, 100, Phaser.Easing.Linear.None, true);
+                        this.levelRestart();
+                    }
+                } else {
+                    game.input.onDown.add(this.beginSwipe, this);;
+                }
+            }     
+        }, this)  
+    },
+
+    // function to check if the level is completed
+    isLevelComplete: function () {
+        for (var i = 0; i < this.rows; i++) {
+            for (var j = 0; j < this.cols; j++) {
+                var tile = this.tilesArray[i][j];
+                if (tile != null && !tile.falling && tile.tileValue != 9) {
+                    return false;
+                }
+            }
+        }
+        return true;     
+    },
+
+    // routine to start when the level is failed
+    levelRestart: function () { 
+        var tween = game.add.tween(this.player).to({
+            alpha: 0,
+            width: gameOptions.tileSize / 4,
+            height: gameOptions.tileSize / 4
+        }, 500, Phaser.Easing.Linear.None, true);
+        tween.onComplete.add(function() {
+            game.state.start('TheGame');
+        }, this);
+    },
+    
+    // ----------------------------------------------------------------------------------------------------
+    
+    convert: function(array) {
+        var grid = [];
+        for (var i = 0; i < this.rows; i++) {
+            for (var j = 0; j < this.cols; j++) {
+                if (array[i] != null && array[i][j] != null && array[i][j].tileValue > 0 && array[i][j].tileValue < 9) {
+                    grid.push(array[i][j].tileValue);
+                } else {
+                    grid.push(0);
+                }
+            }
+        }
+        return grid;
+    },
+    
+    automata: function () {
+        // remove other inputs
+        game.input.onDown.remove(this.beginSwipe, this);
+        game.input.keyboard.removeKey(Phaser.Keyboard.UP);
+        game.input.keyboard.removeKey(Phaser.Keyboard.DOWN);
+        game.input.keyboard.removeKey(Phaser.Keyboard.LEFT);
+        game.input.keyboard.removeKey(Phaser.Keyboard.RIGHT);
+        game.input.keyboard.removeKey(Phaser.Keyboard.SPACE);
+        
+        //
+        var grid = this.convert(this.tilesArray);
+        var startPosition = new Phaser.Point(0, 0);
+        levels[levelNumber].playerPos.clone(startPosition);
+        
+        //
+        var path = AI.ast(grid, this.playerPosition, startPosition, {cols:this.cols, rows:this.rows});
+        console.log(path);
+        
+        if (path) {
+            for (var i = 0; i < path.length; i++) {
+                if (path[i] == 'up') {
+                    setTimeout(this.moveUp.bind(this), 200 * i);
+                } else if (path[i] == 'down') {
+                    setTimeout(this.moveDown.bind(this), 200 * i);
+                } else if (path[i] == 'left') {
+                    setTimeout(this.moveLeft.bind(this), 200 * i);
+                } else if (path[i] == 'right') {
+                    setTimeout(this.moveRight.bind(this), 200 * i);
+                }
+            }
+        } else {
+            alert('I\'m sorry, Dude. I\'m afraid I can\'t do that!');
+        }
+    }
+}
